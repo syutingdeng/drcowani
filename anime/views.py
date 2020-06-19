1491b203576a2f1bbaedaf0043c4321f37f3082b
旭廷 鄧 <billy12349932@gmail.com>
2020-06-19 18:54:33 +0800

    mail

--- anime/views.py
+++ anime/views.py
@@ -34,7 +34,7 @@ def new():
                               result1=newani(1,0),img1=newani(1,1),href1=newani(1,2),
                               result2=newani(2,0),img2=newani(2,1),href2=newani(2,2),
                               result3=newani(3,0),img3=newani(3,1),href3=newani(3,2))
-mail= " "
+
 @app.route('/google_sign_in', methods=['POST'])
 def google_sign_in():
     try:
@@ -44,7 +44,6 @@ def google_sign_in():
         return "ok"
     except:
         return "fail"
-
 @app.route("/animedata")
 def animedata():
     db=sqlite3.connect("./anime/anime.sqlite3")
@@ -81,19 +80,16 @@ def animedata():
     return jsonify(anidata=myanidata,anisrc=myanisrc,aniimg=myaniimg,anilen=myanilen)
 @app.route("/savedata",methods=['POST'])
 def savedata():
-    try:
-        title = str(request.json['title'])
-        link= str(request.json['link'])
-        img= str(request.json['img'])
-        db=sqlite3.connect("./anime/anime.sqlite3")
-        c = db.cursor()
-        c.execute("insert into ani (mail,title,link,img) values (?,?,?,?)",(mail,title,link,img))
-        db.commit()
-        db.close()
-        return "ok"
-    except:
-        print("dataerror")
-        return "fail"
+    title = str(request.json['title'])
+    link= str(request.json['link'])
+    img= str(request.json['img'])
+    db=sqlite3.connect("./anime/anime.sqlite3")
+    c = db.cursor()
+    c.execute("insert into ani (mail,title,link,img) values (?,?,?,?)",(mail,title,link,img))
+    db.commit()
+    db.close()
+    return "ok"
+    
     
 
 
